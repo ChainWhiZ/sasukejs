e2ea7cd3e59138f0bbbf7b95350c84566d225216
Monalisha Mishra <monalisha.mishra@factset.com>
2021-06-14 20:19:48 +0530

    added frontend

--- src/App.js
+++ src/App.js
@@ -1,22 +1,20 @@
-import logo from './logo.svg';
+
 import './App.css';
+import axios from 'axios';
+import { createHashHistory } from 'history'
 
 function App() {
+  
+ function handleLogin(){
+  const history = createHashHistory();
+  window.open(`https://github.com/login/oauth/authorize?client_id=2bcca90edadf4d1f3535`, '_blank');
+  }
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn 
-        </a>
+        
+        <button onClick={handleLogin}>Login</button>
+        
       </header>
     </div>
   );
