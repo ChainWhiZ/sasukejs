43ebe3e78937ce730159953a58e295821fabd92d
Rajashee Parhi <rajashreeparhi23@gmail.com>
2021-06-15 00:25:39 +0530

    frontend

--- src/App.js
+++ src/App.js
@@ -1,38 +1,43 @@
-
-import './App.css';
-import axios from 'axios';
-import { createHashHistory } from 'history';
-import React, { useState, useEffect } from 'react';
+import "./App.css";
+import axios from "axios";
+import { createHashHistory } from "history";
+import React, { useState, useEffect } from "react";
 
 function App() {
-  const [code, setCode] = useState('');
+  const [code, setCode] = useState("");
   useEffect(() => {
-    const url= window.location.href;
+    const url = window.location.href;
     const hasCode = url.includes("?code=");
+
+    //clear code param from url
+
     const clientID = "2bcca90edadf4d1f3535";
     const clientSecret = "1e48b32e22a2a195fafc3702a07c6ca2957f3c0f";
     if (hasCode) {
-      const query = window.location.search.substring(1)
-      const token = query.split('code=')[1]
-      axios({
-        method: "post",
-        url: `https://localhost:4000`,
-        body: {code:hasCode}
-      }).then((response) => {
-        console.log(response);
-      });
+      const query = window.location.search.substring(1);
+      const token = query.split("code=")[1];
+      console.log(token);
+      const requestData = {
+        code: token,
+      };
+      
+      axios
+        .post(`http://localhost:4000/authenticate/user`, { code: token })
+        .then((response) => {
+          console.log(response);
+        });
     }
   });
   function handleLogin() {
     const history = createHashHistory();
-    window.open(`https://github.com/login/oauth/authorize?client_id=2bcca90edadf4d1f3535`, '_blank');
+    window.open(
+      `https://github.com/login/oauth/authorize?client_id=2bcca90edadf4d1f3535`
+    );
   }
   return (
     <div className="App">
       <header className="App-header">
-
         <button onClick={handleLogin}>Login</button>
-
       </header>
     </div>
   );
