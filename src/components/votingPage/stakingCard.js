09e2530069bf910c783906ae2ecf61cec35d869a
Monalisha Mishra <mishramonalisha76@gmail.com>
2021-07-09 22:57:42 +0530

    staking page

--- src/components/votingPage/stakingCard.js
+++ src/components/votingPage/stakingCard.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React,{useState} from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
@@ -34,8 +34,11 @@ const useStyles = makeStyles({
 
 export default function StakingCard(props) {
   const classes = useStyles();
+  const [stakedAmount,setStakedAmount]= useState('');
+  const [balance,setBalance]= useState(270);
+  const handleStake =()=>{
 
-
+  }
   return (
     <>
 
@@ -56,11 +59,11 @@ export default function StakingCard(props) {
        
       </div>
       <div className={classes.stakeDiv}>
-        <TextField id="outlined-basic" variant="outlined" size="small" />
+        <TextField id="outlined-basic" variant="outlined" size="small" value={stakedAmount} onChange={e=>setStakedAmount(e.target.value)}/>
         <br />
         <br />
-        <Button variant="contained">Stake Now</Button>
-        <p>Avbl. Balance- 230 MATIC</p>
+        <Button variant="contained" onClick={()=>handleStake()}>Stake Now</Button>
+        <p>Avbl. Balance- {balance} MATIC</p>
       </div>
 
     </>
