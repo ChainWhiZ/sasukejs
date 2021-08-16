4bf255fb1fd9768ddd256d5afb0c9edf1fda27eb
Rajashee Parhi <rajashreeparhi23@gmail.com>
2021-08-17 00:42:25 +0530

    ques page

--- src/components/questionPost/questionPostCss.js
+++ src/components/questionPost/questionPostCss.js
@@ -1,22 +1,21 @@
 import { makeStyles } from "@material-ui/core/styles";
+import { Label } from "@material-ui/icons";
 
 export const useStyles = makeStyles((theme) => ({
-    root: {
-        flexGrow: 1,
-        margin:"4%",
-        fontFamily:"Poppins"
-
-    },
-    heading: {
-        textAlign: "center"
-    },
-    marginLeftRight10:{
-        marginLeft: "15%",
-        marginRight:"15%"
-    },
-    paper: {
-        padding: theme.spacing(2),
-        marginRight: theme.spacing(12),
-        textAlign: 'center',
-      }
+  root: {
+    flexGrow: 1,
+    margin: "4%",
+  },
+  heading: {
+    textAlign: "center",
+  },
+  marginLeftRight10: {
+    marginLeft: "15%",
+    marginRight: "15%",
+  },
+  paper: {
+    padding: theme.spacing(2),
+    marginRight: theme.spacing(12),
+    textAlign: "center",
+  },
 }));
