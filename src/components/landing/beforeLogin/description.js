e3396e10602ff81068a109ec08ca980d22e9cd98
akp111 <pradhanashish95@gmail.com>
2021-10-05 22:02:22 +0530

    login

--- src/components/landing/beforeLogin/description.js
+++ src/components/landing/beforeLogin/description.js
@@ -1,42 +1,42 @@
-import React, { useEffect } from "react";
-import axios from "axios";
-import { Link } from "react-router-dom";
-import Grid from "@material-ui/core/Grid";
-import { port } from "../../../config/config";
-import Button from "@material-ui/core/Button";
-import logo from "../../../assets/cwz.png";
-import "../landing.css";
-import LoginButtons from "./loginButtons";
+// import React, { useEffect } from "react";
+// import axios from "axios";
+// import { Link } from "react-router-dom";
+// import Grid from "@material-ui/core/Grid";
+// import { port } from "../../../config/config";
+// import Button from "@material-ui/core/Button";
+// import logo from "../../../assets/cwz.png";
+// import "../landing.css";
+// import LoginButtons from "./loginButtons";
 
-export default function Description() {
-  return (
-    <>
-      <Grid container >
-        <Grid item xs={12} md={12}>
-          <Link to="/">
-            <img src={logo} alt="logo" />
-          </Link>
-        </Grid>
+// export default function Description() {
+//   return (
+//     <>
+//       <Grid container >
+//         <Grid item xs={12} md={12}>
+//           <Link to="/">
+//             <img src={logo} alt="logo" />
+//           </Link>
+//         </Grid>
 
-        <Grid item md={12} xs={12} class="margin-left10">
-          <p class="landing-heading">A decentralized and an open-source marketplace for dapps</p>
-        </Grid>
-        <Grid item md={12} xs={12} class="margin-left10">
-          <p style={{marginRight: "-7%"}}>
-            <span class="pointer">.</span>
-            Post a bounty and hire a freelancer to build your dapp </p>
-          <p style={{marginRight: "-7%"}}>
-            <span class="pointer">.</span>
-            Solve open source bounties and earn bounty rewards </p>
-          <p style={{marginRight: "-10%"}}>
-            <span class="pointer" >.</span>
-            Vote on solutions and earn incentives on staked tokens</p>
-            <br/>
-        </Grid>
-        <Grid item md={12} xs={12}>
-          <LoginButtons />
-        </Grid>
-      </Grid>
-    </>
-  );
-}
+//         <Grid item md={12} xs={12} class="margin-left10">
+//           <p class="landing-heading">A decentralized and an open-source marketplace for dapps</p>
+//         </Grid>
+//         <Grid item md={12} xs={12} class="margin-left10">
+//           <p style={{marginRight: "-7%"}}>
+//             <span class="pointer">.</span>
+//             Post a bounty and hire a freelancer to build your dapp </p>
+//           <p style={{marginRight: "-7%"}}>
+//             <span class="pointer">.</span>
+//             Solve open source bounties and earn bounty rewards </p>
+//           <p style={{marginRight: "-10%"}}>
+//             <span class="pointer" >.</span>
+//             Vote on solutions and earn incentives on staked tokens</p>
+//             <br/>
+//         </Grid>
+//         <Grid item md={12} xs={12}>
+//           <LoginButtons />
+//         </Grid>
+//       </Grid>
+//     </>
+//   );
+// }
--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -6,7 +6,10 @@ import Toolbar from "@material-ui/core/Toolbar";
 import Button from "@material-ui/core/Button";
 import Grid from "@material-ui/core/Grid";
 import { Link } from "react-router-dom";
+import GithubButton from 'react-github-login-button'
+import axios from "axios"
 import logo from "../../assets/cwz.png";
+import { port } from "../../config/config";
 import AccountCircle from "@material-ui/icons/AccountCircle";
 
 export default function Navbar(props) {
@@ -14,6 +17,7 @@ export default function Navbar(props) {
   let [username, setUsername] = useState(localStorage.getItem("username"));
 
   const [hover, setHover] = useState(false);
+  const [clicked, setClicked] = useState(false);
   const onHover = () => {
     setHover(true);
   };
@@ -23,6 +27,24 @@ export default function Navbar(props) {
   let usernameTruncated;
   if (username && username.length > 11) {
     usernameTruncated = username.substring(0, 11) + "...";
+  }
+
+  const githubAuth = () => {
+    const url = window.location.href;
+    const hasCode = url.includes("?code=");
+    if (hasCode) {
+      const query = window.location.search.substring(1);
+      const token = query.split("code=")[1];
+
+      axios
+        .post(port + "authenticate/user", { code: token })
+        .then((response) => {
+          console.log(response)
+          localStorage.setItem("username", response.data.doc.githubId);
+          window.history.pushState({}, {}, "/landing");
+          window.location.reload();
+        });
+    }
   }
 
   return (
@@ -38,8 +60,8 @@ export default function Navbar(props) {
               </Link>
             </Grid>
           </Grid>
-          <Grid container>
-            <Grid item md={2} xs={12}>
+          <Grid container direction="row" style={{marginLeft:"-10%"}}>
+            <Grid item md={3} xs={12}>
               <Link to="/post" className={classes.link}>
                 <Button
                   className={classes.button}
@@ -50,7 +72,7 @@ export default function Navbar(props) {
                 </Button>
               </Link>
             </Grid>
-            <Grid item md={2} xs={12}>
+            <Grid item md={3} xs={12}>
               <Link
                 to={{
                   pathname: "/explore",
@@ -65,11 +87,11 @@ export default function Navbar(props) {
                   style={{ textTransform: "none" }}
                   color="inherit"
                 >
-                  Solve a Bounty
+                  Solve Bounty
                 </Button>
               </Link>
             </Grid>
-            <Grid item md={2} xs={12}>
+            <Grid item md={3} xs={12}>
               <Link
                 to={{
                   pathname: "/explore",
@@ -88,7 +110,7 @@ export default function Navbar(props) {
                 </Button>
               </Link>
             </Grid>
-             <Grid item md={3} xs={12}>
+            {/* <Grid item md={2} xs={12}>
               <Link
                 to={{
                   pathname: "/explore",
@@ -106,29 +128,53 @@ export default function Navbar(props) {
                   Completed Questions
                 </Button>
               </Link>
-            </Grid>
-              <>
-                <Grid
-                  item
-                  md={2}
-                  xs={12}
-                  onMouseEnter={onHover}
-                  onMouseLeave={onLeave}
-                >
-                  <p className={classes.username}>
-                    {hover && username && username.length > 18
-                      ? username
-                      : username && username.length > 18
-                      ? usernameTruncated
-                      : username}
-                  </p>
-                </Grid>
-                <Grid item md={1} xs={12}>
-                  <Link to="/profile" className={classes.link}>
-                    <AccountCircle className={classes.accountIcon} />
-                  </Link>
-                </Grid>
-              </>
+            </Grid> */}
+            <>
+              {/* {
+                clicked && username ?
+                  (
+                    <Grid
+                      item
+                      md={2}
+                      xs={12}
+                      onMouseEnter={onHover}
+                      onMouseLeave={onLeave}
+                    >
+                      <p className={classes.username}>
+                        {hover && username && username.length > 18
+                          ? username
+                          : username && username.length > 18
+                            ? usernameTruncated
+                            : username}
+                      </p>
+                    </Grid>
+
+                  ) :
+                  (
+                    <p></p>
+                  )
+              } */}
+
+              <Grid item md={1} xs={12}>
+                {
+                  username ? (
+                    <Link to="/profile" className={classes.link}>
+                      <AccountCircle className={classes.accountIcon} />
+                    </Link>
+                  ) : (
+                    <>
+                      <a href="https://github.com/login/oauth/authorize?client_id=2bcca90edadf4d1f3535">
+                        <GithubButton style={{width:"7vw"}} label="" onClick={() => {
+                          githubAuth()
+                        }} />
+                      </a>
+                    </>
+
+                  )
+
+                }
+              </Grid>
+            </>
           </Grid>
         </Toolbar>
       </AppBar>
