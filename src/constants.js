a519500145f728cff8b7fe54cd4b7c3100705aaf
Rajashree Parhi <rajashree.parhi@factset.com>
2021-12-25 15:44:47 +0530

    remove lorem ipsum

--- src/constants.js
+++ src/constants.js
@@ -31,74 +31,79 @@ export const drawerList = [
   "Voting",
 ];
 
-
 export const text = {
   page1: {
-      title: `What should we call your Issue?`,
-      content: `The title is the first thing that a dev sees when he/she comes across your bounty. It’s best practise to keep the title closely related to the issue. Avoid vague words like Important, amazing, stunning etc.`
+    title: `What should we call your Issue?`,
+    content: `The title is the first thing that a dev sees when he/she comes across your bounty. It’s best practise to keep the title closely related to the issue. Avoid vague words like Important, amazing, stunning etc.`,
   },
   page2: {
-      title: `Choose the category/ies`,
-      content: `The category of an issue helps the devs
-      associate it with their areas of strengths. Defining the proper set of categories is important for ensuring quality over quantity as solutions. `
+    title: `Choose the category/ies`,
+    content: `The category of an issue helps the devs
+      associate it with their areas of strengths. Defining the proper set of categories is important for ensuring quality over quantity as solutions. `,
   },
   page3: {
-      title: `Expected time of the solution`,
-      content: `Use this section to highlight the number of days within which you are expecting the bounty to be completed. Remember, shorter the expected time, higher should be the bounty amount. `
+    title: `Expected time of the solution`,
+    content: `Use this section to highlight the number of days within which you are expecting the bounty to be completed. Remember, shorter the expected time, higher should be the bounty amount. `,
   },
   page4: {
-      title: `Provide the Github issue link`,
-      content: `Provide the link to the issue on your Github for which you’re posting this bounty. Please do not post a spam link or a shortened link. The community checks for spam bounties and can report you for acting as a bad actor.`
+    title: `Provide the Github issue link`,
+    content: `Provide the link to the issue on your Github for which you’re posting this bounty. Please do not post a spam link or a shortened link. The community checks for spam bounties and can report you for acting as a bad actor.`,
   },
   page5: {
-      title: `Provide the bounty amount`,
-      content: `Mention the amount that the developer
-      shall receive after completing the bounty. Note: This amount will be held in an escrow contract and will be transferred to the developer once you confirm that you’ve received the code.`
+    title: `Provide the bounty amount`,
+    content: `Mention the amount that the developer
+      shall receive after completing the bounty. Note: This amount will be held in an escrow contract and will be transferred to the developer once you confirm that you’ve received the code.`,
   },
   page6: {
-      title: `Who can approve the solution`,
-      content: `The Issue title is the first thing that a 
-      developer sees when he comes across your bounty. It’s best practise to keep the title to the point and closely related to the issue. Avoid vague words like Important, Amazing, Stunning etc.`
+    title: `Who can approve the solution`,
+    content: `The Issue title is the first thing that a 
+      developer sees when he comes across your bounty. It’s best practise to keep the title to the point and closely related to the issue. Avoid vague words like Important, Amazing, Stunning etc.`,
   },
   page7: {
-      title: `Enter Community Reward`,
-      content: `Community voting makes your life a lot easier in selecting the best solution to your bounty. They do the voting.You choose the best solution based on the votes. Simple and Fast. The reward goes out to the voters who voted for the highest voted solutions. `
+    title: `Enter Community Reward`,
+    content: `Community voting makes your life a lot easier in selecting the best solution to your bounty. They do the voting.You choose the best solution based on the votes. Simple and Fast. The reward goes out to the voters who voted for the highest voted solutions. `,
   },
   page8: {
-      title: `Confirm your Wallet Address`,
-      content: `Provide the wallet address which you will be using for transfering the bounty reward to the escrow. All transactions, regarding the bounty shall be associated with this address. `
-  }
-}
+    title: `Confirm your Wallet Address`,
+    content: `Provide the wallet address which you will be using for transfering the bounty reward to the escrow. All transactions, regarding the bounty shall be associated with this address. `,
+  },
+};
 
 export const categoryText = [
   {
-      title: "Front End",
-      content: "Front-end web development is the development of the graphical user interface of a website, through the use of HTML, CSS, and JavaScript, so that users can view and interact with that website."
+    title: "Front End",
+    content:
+      "Front-end web development is the development of the graphical user interface of a website, through the use of HTML, CSS, and JavaScript, so that users can view and interact with that website.",
   },
   {
-      title: "Back End",
-      content: "Backend is the server-side of the website. It stores and arranges data, and also makes sure everything on the client-side of the website works fine. Its basically behind the scenes."
+    title: "Back End",
+    content:
+      "Backend is the server-side of the website. It stores and arranges data, and also makes sure everything on the client-side of the website works fine. Its basically behind the scenes.",
   },
   {
-      title: "Smart Contract",
-      content: "A smart contract is a self-executing contract with the terms of the agreement between buyer and seller being directly written into lines of code. The code exists on the blockchain network."
+    title: "Smart Contract",
+    content:
+      "A smart contract is a self-executing contract with the terms of the agreement between buyer and seller being directly written into lines of code. The code exists on the blockchain network.",
   },
   {
-      title: "Others",
-      content: "This is an open category that can correspond to a vast number of options like documentation, design, architecture or any other categories that we missed out on."
-  }
-]
+    title: "Others",
+    content:
+      "This is an open category that can correspond to a vast number of options like documentation, design, architecture or any other categories that we missed out on.",
+  },
+];
 
 export const communityText = [
   {
-      title: "Community Approved",
-      content: "There are many variations of passs of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look asa o la sl asosl saseo w wls also l asl a lwew pals asl as"
+    title: "Community Approved",
+    content:
+      "The community stakes and votes on the submitted solutions to your bounty and helps you figure out the best solution.You get to your desired solution 5x faster. The community earns incentives. Win-win.",
   },
   {
-      title: "Self Approved",
-      content: "There are many variations of passs of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look asa o la sl asosl saseo w wls also l asl a lwew pals asl as"
+    title: "Self Approved",
+    content:
+      "If you are willing to spend hours reviewing pieces of code to figure out the best solution to your bounty, this one is  for you. Pros: You are superhuman. Cons: You don’t leverage the power of community. Win-Lose.   ",
   },
-]
+];
 
 export const sidebarItems = [
   {
@@ -117,4 +122,4 @@ export const sidebarItems = [
     name: "Your Profile",
     url: "/profile",
   },
-]
+];
