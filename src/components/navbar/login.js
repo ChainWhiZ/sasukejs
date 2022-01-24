2556c477b229234b1079000249e68b0c15dac2a9
Rajashee Parhi <rajashreeparhi23@gmail.com>
2022-01-24 23:16:22 +0530

    css

--- src/components/navbar/login.js
+++ src/components/navbar/login.js
@@ -8,6 +8,7 @@ import "./navbar.css";
 
 export default function Login() {
   const [username, setUsername] = useRecoilState(usernameAtom);
+  setUsername("voter5")
   const [ loader,setLoader] = useState(false);
   setUsername("mishramonalisha76");
   useEffect(() => {
