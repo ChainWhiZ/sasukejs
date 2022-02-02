3305275a8aa70cbacd2357f4904c4ff3b099a765
Monalisha Mishra <monalisha.mishra@factset.com>
2022-02-02 16:05:32 +0530

    added config

--- src/config/config.js
+++ src/config/config.js
@@ -1,2 +1,2 @@
- export const port = "https://chainwhiz.herokuapp.com/"
+ export const port = "http://ec2-18-218-62-149.us-east-2.compute.amazonaws.com:3000/"
  //export const port = "http://localhost:4000/"
