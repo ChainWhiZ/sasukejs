4a879376b3b0b5484ccb9f597e38f236c555b6fc
Monalisha Mishra <monalisha.mishra@factset.com>
2022-03-23 01:25:58 +0530

    new ui change made

--- src/components/profilePageNew/questionDetail.js
+++ src/components/profilePageNew/questionDetail.js
@@ -1,9 +1,24 @@
 import React from "react";
 import Grid from "@material-ui/core/Grid";
 import GithubIcon from "../../assets/githubIcon.png";
+import VideoIcon from "../../assets/video.png";
+import LinkIcon from "../../assets/link-icon.png";
 import "./profilePageCss.css";
 
 export default function QuestionDetail(props) {
+  const handleLinkDisable = () =>{
+    if(props.issueUrl.includes('https://github.com/'))
+    
+    return true;
+    else
+    return false;
+   }
+   const handleLinkDisableClass = (flag) =>{   
+     if(flag) 
+     return "link heading-text bounty-middle-github bounty-middle-github-disabled";
+     return "link heading-text bounty-middle-github";
+    }
+ 
   return (
     <>
       <Grid container className="profile-question-detail-grid">
@@ -36,10 +51,7 @@ export default function QuestionDetail(props) {
         <Grid item md={12}>
           <p className="profile-text-style">Resources/Links</p>
         </Grid>
-        <Grid item md={2}>
-          <img class="icon" src={GithubIcon} alt="git" style={{marginTop:"-2%"}} />
-        </Grid>
-        <Grid item md={10}>
+        {/* <Grid item md={10}>
           <a
             href={props.githubIssueUrl}
             target="_blank"
@@ -47,6 +59,38 @@ export default function QuestionDetail(props) {
             className="profile-content-style profile-pae-github"
           >
             Github Repo
+          </a>
+        </Grid> */}
+        <Grid item md={4}>
+          <img class={!handleLinkDisable()?"bounty-middle-github-disabled":""} src={GithubIcon} alt="git" />
+          <a
+            class={handleLinkDisableClass(!handleLinkDisable())}
+            href={props.issueUrl}
+            target="_blank"
+            rel="noreferrer"
+          >
+            Issue Link
+          </a>
+        </Grid>
+        <Grid item md={4}>
+          <img class={handleLinkDisable()?"bounty-middle-github-disabled":""} src={LinkIcon} alt="git" />
+          <a
+            class={handleLinkDisableClass(handleLinkDisable())}
+            href={props.issueUrl}
+            target="_blank"
+            rel="noreferrer"
+          >
+           Other Link
+          </a>
+        </Grid>
+        <Grid item md={4}>
+          <img class="bounty-middle-github-disabled" src={VideoIcon} alt="git" />
+          <a
+            class="link heading-text bounty-middle-github bounty-middle-github-disabled"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Video
           </a>
         </Grid>
       </Grid>
