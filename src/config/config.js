d4b59f49c8006411661f66ecb863d03b57d12219
akp111 <pradhanashish95@gmail.com>
2022-04-19 13:54:07 +0530

    changed to mumbai

--- src/config/config.js
+++ src/config/config.js
@@ -1,5 +1,5 @@
-//export const port = "https://backend-app.chainwhiz.app/"
-export const port = "http://localhost:4000/"
+export const port = "https://backend-app.chainwhiz.app/"
+// export const port = "http://localhost:4000/"
 
  export const new_backend_port="https://backend-prod.chainwhiz.app/"
 //  export const new_backend_port="http://localhost:4001/"
